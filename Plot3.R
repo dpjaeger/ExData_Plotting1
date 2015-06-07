41122289d4efc3273111ef68c62607aff115b8f2
dpjaeger <dpjaeger@gmail.com>
2015-06-07 19:50:15 -0300

    Commit3
    Commit3

--- Plot3.R
+++ Plot3.R
@@ -1,16 +1,21 @@
 ##To be run after Plot1.R and Plot2.R
 ##Chart3
 
+##Melt the dataset first to create this plot
+pow2 <- pow[ ,c(7,8,9,11)]
+pow2$datetime <- as.POSIXct(pow2$datetime)
+pow2 <- melt(pow2, id="datetime")
+
 opar = par(ps = 10)
 png(filename = "plot3.png", width = 480, height = 480)
-Chart3 <- ggplot(data = pow, aes(x = datetime))
-Chart3 <- Chart3 + geom_line(aes(y = Sub_metering_1), colour = "black", ylab = "Test")
-Chart3 <- Chart3 + geom_line(aes(y = Sub_metering_2), colour = "red", ylab = "Test2")
-Chart3 <- Chart3 + geom_line(aes(y = Sub_metering_3), colour = "blue", ylab = "Test3")
+Chart3 <- ggplot(data = pow2, aes(x = datetime, y=value, group=variable, colour=variable))
+Chart3 <- Chart3 + geom_line()
 Chart3 <- Chart3 + xlab("") + ylab("Energy sub metering")
 Chart3 <- Chart3 + scale_x_datetime(labels = date_format("%a"), breaks = date_breaks("1 day"))
 Chart3 <- Chart3 + theme_bw()
 Chart3 <- Chart3 + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-Chart3 <- Chart3 + guides(col = guide_legend)
+Chart3 <- Chart3 + scale_color_manual(values = c("Black", "Red", "Blue"))
+Chart3 <- Chart3 + theme(legend.title=element_blank())
+Chart3 <- Chart3 + theme(legend.position=c(.80, .85))
 Chart3
 dev.off()
